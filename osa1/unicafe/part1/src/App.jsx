5e99384fb8923e1ffc65315834bf2d41f641d99e
Hannes Vieth <hannesvieth@gmail.com>
2025-01-16 21:59:35 +0200

    tehtava 1.6 - unicafe aloitettu

---
+++ osa1/unicafe/part1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      code here
+      <h1>statistics</h1>
+      <p>good</p>
+      <p>neutral</p>
+      <p>bad</p>
+    </div>
+  )
+}
+
+export default App
