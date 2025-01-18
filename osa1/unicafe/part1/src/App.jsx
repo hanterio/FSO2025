25e9b5c2895a536303627e0affda24f8e3882c33
Hannes Vieth <hannesvieth@gmail.com>
2025-01-18 20:24:22 +0200

    tehtavat 1.6 ja 1.7 tehty - uusi commit koska vanha meni väärään paikkaan

--- osa1/unicafe/part1/src/App.jsx
+++ osa1/unicafe/part1/src/App.jsx
@@ -6,14 +6,38 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const [total, setTotal] = useState(0)
+  
+  const handleGoodClick = () => {
+    const updatedGood = good +1
+    setGood(updatedGood)
+    setTotal(updatedGood + neutral + bad)
+  }
+  const handleNeutralClick = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    setTotal(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    setTotal(good + neutral + updatedBad)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
-      code here
+      <button onClick={handleGoodClick}>good</button>
+      <button onClick={handleNeutralClick}>neutral</button>
+      <button onClick={handleBadClick}>bad</button>
       <h1>statistics</h1>
-      <p>good</p>
-      <p>neutral</p>
-      <p>bad</p>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {total}</p>
+        <p>average {(good - bad)/total}</p>
+        <p>positive {good*100/total} %</p>
     </div>
   )
 }
